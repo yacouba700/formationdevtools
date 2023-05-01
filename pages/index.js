6bdd7d7d4aabe58ed5b20f4f0e5cfb6b8e5581a0
yacouba700 <108770173+yacouba700@users.noreply.github.com>
2023-05-01 22:49:51 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
             <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
-          <h1>S'inscrire</h1>
+          
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
