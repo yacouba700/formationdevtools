f39ab0a369ed8946abcf81a1af9cd8cfe8fdbb3a
yacouba700 <108770173+yacouba700@users.noreply.github.com>
2023-05-01 23:01:13 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p onClick={()=>{return fetch("https://formationapp.onrender.com/scrape")}}>
-            Get started by editing&nbsp;
+            Get !! started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
